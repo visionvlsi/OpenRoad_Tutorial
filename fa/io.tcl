2a3b90a2b031b6d5f44f52967a085410e97b9f19
Vision-VLSI Industry Training Institute <vvi.pvtltd@gmail.com>
2023-12-26 16:58:11 +0530

    Update io.tcl

--- fa/io.tcl
+++ fa/io.tcl
@@ -8,10 +8,5 @@
 clear_io_pin_constraints
 set_io_pin_constraint -region left:* -pin_names {a b cin}
 set_io_pin_constraint -region bottom:* -pin_names {clk}
-#set_io_pin_constraint -pin_names a -region left
-#set_io_pin_constraint -pin_names b -region left
-#set_io_pin_constraint -pin_names cin -region left
 
-set_io_pin_constraint -region right:* -pin_names {sum co} 
-#set_io_pin_constraint -pin_names sum -region right
-#set_io_pin_constraint -pin_names co -region right
+set_io_pin_constraint -region right:* -pin_names {sum co}
